eb51e36fd0708e3bed906d9118d94797e42c5a8b
Steven Topazio <sat@northpointprinting.com>
2020-03-09 15:09:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name}."
 end
+
+def intorduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning "
